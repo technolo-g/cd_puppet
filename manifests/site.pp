af41ef261b87502216f1a82205dda1a42dd96c90
Matt Bajor <mbajor@pingidentity.com>
2014-06-19 21:25:04 -0600

    Added new classes to site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,3 +1,17 @@
 node default {
   include ntp
+
+  class { 'python':
+    version    => 'system',
+    dev        => true,
+    pip        => true
+  }
+
+  zk_watcher::add { 'ntp':
+    port    => '123',
+    cmd     => 'pgrep ntp',
+    path    => '/services/production/uswest1/ntp',
+    data    => ['zone=uswest1a', 'user=mbajor'],
+    refresh => '60';
+  }
 }
