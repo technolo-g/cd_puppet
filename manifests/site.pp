f279ca0bdfafbc14d5cf753f1e14211db66e11f9
Matt Bajor <mbajor@pingidentity.com>
2014-06-19 22:41:42 -0600

    Document the demo

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,17 +1,34 @@
 node default {
+  # Configure ntp, the main service
   include ntp
 
+  # Include the python class for Pip and as
+  # a req for zk_watcher
   class { 'python':
     version    => 'system',
     dev        => true,
     pip        => true
   }
 
+  # Install zk_watcher on ntp
   zk_watcher::add { 'ntp':
     port    => '123',
     cmd     => 'pgrep ntp',
-    path    => "/services/production/${::certname}/ntp",
+    path    => "/services/production/ntp",
     data    => ['zone=uswest1a', 'user=mbajor'],
     refresh => '60';
   }
+
+  # Retrieve the list of servers
+  $servers = zkget('/services/production/ntp', 0, 1)
+
+  # Display the list of servers
+  notify { "There are $servers in the cluster": }
+
+  # Drop a file in /tmp with the nodes
+  file { '/tmp/clusternodes':
+    ensure  => present,
+    content => template('zk_watcher/cluster.erb')
+  }
+
 }
